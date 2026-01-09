6df663f3323e92385f646d96b6c8f94173dae412
JohnsCodingLab <engrlevijohn@gmail.com>
2026-01-09 23:59:07 +0100

    openapi file

---
+++ src/shared/docs/openapi.ts
@@ -0,0 +1,22 @@
+import {
+  OpenAPIRegistry,
+  OpenApiGeneratorV3,
+} from "@asteasolutions/zod-to-openapi";
+import { z } from "zod";
+
+export const registry = new OpenAPIRegistry();
+
+// Utility to generate the final documentation object
+export const getOpenApiDocumentation = () => {
+  const generator = new OpenApiGeneratorV3(registry.definitions);
+
+  return generator.generateDocument({
+    openapi: "3.0.3",
+    info: {
+      title: "TSCommerce API",
+      version: "1.0.0",
+      description: "E-commerce REST API built with Node.js & TypeScript",
+    },
+    servers: [{ url: "/api/v1" }],
+  });
+};
